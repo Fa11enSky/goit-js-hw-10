22f479e1264656f7bb1a2c959f0765b0b106bd2f
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2023-10-01 21:10:11 +0300

    full hw10

---
+++ src/index.js
@@ -0,0 +1,95 @@
+// api key: live_zjktP70bUEcCwtKHt678pfCqUHuR5MGDhhd1Id3WRSMDaX7PH9iv5scgUh3N6TwV
+//End poin: https://api.thecatapi.com/v1/breeds
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js';
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css'
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_zjktP70bUEcCwtKHt678pfCqUHuR5MGDhhd1Id3WRSMDaX7PH9iv5scgUh3N6TwV';
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  errorMessage: document.querySelector('.error'),
+  container: document.querySelector('.cat-info'),
+};
+refs.select.addEventListener('change', handleChange);
+
+
+
+
+
+
+
+start(fetchBreeds);
+
+// ! Стартова функція, яка викликає всі інші приймає як колбек функію яка повертає проміс
+function start(fetchFunction) {
+  fetchFunction()
+    .then(createMarkup).catch(error => {
+      Notiflix.Notify.failure(error.message);
+    })
+    .finally(() => {
+      visibilityControl(refs.loader, 'none')
+    slimSelect = new SlimSelect({
+  select: refs.select
+})});
+  
+}
+
+//! створення полів в селекті
+function createMarkup(obj) {
+  const markup = obj
+    .map(el => {
+      return `
+          <option value="${el.id}">${el.name}</option>`;
+    })
+    .join('');
+  refs.select.innerHTML = markup;
+  // видимість додаю тут бо якщо додавати після промісу в кінець, то воно відпрацьовує
+  // коли розмітка ще не відмальована
+  refs.select.classList.remove('visually-hidden');
+}
+
+
+
+//! контроль видимості завантаження
+function visibilityControl(loader, status) {
+  loader.style.display = status;
+}
+
+//! слухач для поля селект
+//! Викликає дві функції, перша отримує id для пошуку повної інформації
+//! Друга створює блок, з обєкта який повертає перша
+function handleChange(ev) {
+  console.log(ev.target.value);
+  visibilityControl(refs.loader, 'block');
+  fetchCatByBreed(ev.target.value)
+    .then(data => {
+      createCatBlock(data, refs.container);
+    })
+    .then(() => {
+      visibilityControl(refs.loader, 'none');
+    })
+    .catch(error => {
+      visibilityControl(refs.loader,'none')
+      Notiflix.Notify.failure(error.message)
+    });
+}
+
+//! Для створення блоку з інформацією про котана
+//! Приймає об'єкт і блок в який потрібно вставити розмітку
+function createCatBlock(catObj, blockToInner) {
+  const markup = `
+  <div class="thumb"><img src="${catObj.url}" alt="${catObj.breeds[0].name}" width="${catObj.breeds[0].width}" height="${catObj.breeds[0].height}"></div>
+ <div class="description">
+   <h2>${catObj.breeds[0].name}</h2> 
+   <p>${catObj.breeds[0].description}</p>
+   <p><strong>Temperament:</strong>${catObj.breeds[0].temperament}</p>
+    
+ </div>`;
+  blockToInner.innerHTML = markup;
+}
+
---
+++ src/js/cat-api.js
@@ -0,0 +1,33 @@
+function fetchBreeds() {
+  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
+    if (!response.ok) {
+      throw new Error('Oops! Something went wrong! Try reloading the page!(first fetch)');
+    }
+    return response.json();
+  });
+}
+
+// Отримує повну інформацію про котана через два запити:
+// перший щоби дізнатись ідентифікатор
+// другий щоби отримати обєкт з інформацією
+function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the(Помилка при отримані ID)');
+      }
+      return response.json();
+    }).then(data => {
+      return fetch(`https://api.thecatapi.com/v1/images/${data[0].id}`)
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the (Помилка при отримані вмісту)');
+      }
+      return response.json()
+    })
+    
+}
+
+export { fetchBreeds, fetchCatByBreed };
